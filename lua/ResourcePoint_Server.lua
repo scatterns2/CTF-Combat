76b4e56c3103095e1bf866a3518e38c00211a911
SoulRider <andy@internetaddress.com>
2015-01-28 23:22:42 +0000

    Flag
    Added flag back in.

--- lua/ResourcePoint_Server.lua
+++ lua/ResourcePoint_Server.lua
@@ -55,7 +55,7 @@ function ResourcePoint:SpawnFlagForTeam(team)
     if self:GetAttached() == nil then
     
         // Force create because entity may not be cleaned up from round reset
-        local flag = CreateEntityForTeam(kTechId.SentryBattery, self:GetOrigin(), team:GetTeamNumber(), nil)
+        local flag = CreateEntityForTeam(kTechId.Flag, self:GetOrigin(), team:GetTeamNumber(), nil)
         
         if flag then      
             
